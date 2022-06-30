27eed08e37a95a5b7b35198e8cf870cb64c0ca19
ashutoshnawale89 <107869924+ashutoshnawale89@users.noreply.github.com>
2022-06-30 07:50:33 +0530

    Update WELCOMETOJAVA.java

--- assignent2JAVA/src/assignent2JAVA/WELCOMETOJAVA.java
+++ assignent2JAVA/src/assignent2JAVA/WELCOMETOJAVA.java
@@ -2,7 +2,7 @@ package assignent2JAVA;
 
 public class WELCOMETOJAVA {
 	public static void main (String [] argss) {
-		System.out.println("WELCOMETOJAVA for package assig,2java");
+		System.out.println("WELCOMETOJAVA2 PROGRAM");
 	}
 
 }
